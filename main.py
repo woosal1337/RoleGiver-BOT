f2953c4a6c65b0a54f43e4bb4e83fd694b4f7618
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-12-24 08:45:44 +0300

    FINAL

--- main.py
+++ main.py
@@ -31,20 +31,21 @@ allNames = sortNames.sort("teamNames.txt")
 
 @bot.event
 async def on_ready():
-    print("Kodluyoruz HACKATHON is ready to go!")
+    global commonNumberCount
+
     kodluyoruzChannel = bot.get_channel(746850985510699092)
 
-
-    for z in allNames:
-        nameCounter = 0
+    for i in range(len(allNames)):
+        nameCounter = 1
         currentTeamMessage = "```\n"
-
-        for j in z:
+        commonNumberCount = len(allNames[i])
+        
+        for j in allNames[i]:
             print(j)
 
-            currentTeamMessage += f"{}"
-            currentTeamMessage += j
+            currentTeamMessage += (f"{nameCounter} : " + j)
             currentTeamMessage += "\n"
+            nameCounter += 1
 
         currentTeamMessage += "```"
         await kodluyoruzChannel.send(currentTeamMessage)
@@ -52,9 +53,11 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    global commonNumberCount
+
     if (message.channel.id == 746850985510699092):
-        for i in oneToTenEmojis:
-            await message.add_reaction(i)
+        for i in range(0, commonNumberCount):
+            await message.add_reaction(oneToTenEmojis[i])
     else:
         pass
 
